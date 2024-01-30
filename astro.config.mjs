cb13e9d22359dc1642a19c60fe0cc3c8836c845f
bbag <bill.baggerman@gmail.com>
2024-01-29 22:13:44 -0500

    Added shiki code highlighting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
-import { defineConfig } from 'astro/config';
-import vue from "@astrojs/vue";
-
-import mdx from "@astrojs/mdx";
+import { defineConfig } from 'astro/config'
+import vue from "@astrojs/vue"
+import mdx from "@astrojs/mdx"
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,6 +10,12 @@ export default defineConfig({
     },
     integrations: [vue(), mdx()],
     markdown: {
-        syntaxHighlight: 'prism'
+        syntaxHighlight: 'shiki',
+        shikiConfig: {
+            theme: 'material-theme-palenight', // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+            langs: [], // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+            wrap: true, // Enable word wrap to prevent horizontal scrolling
+            transformers: [] // See https://shikiji.netlify.app/guide/transformers and https://shikiji.netlify.app/packages/transformers
+        }
     }
-});
+})
